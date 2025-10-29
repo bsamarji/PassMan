c2c90d821b9426cda60bb443fd512e02183e9e06
bsamarji <bensamarji5637@gmail.com>
2025-10-29 11:30:19 +0000

    Added salt generation and kdf definition

--- src/passman/security.py
+++ src/passman/security.py
@@ -18,3 +18,27 @@ def generate_salt_file():
         with open(salt_file, "wb") as f:
             f.write(salt)
 
+def retrieve_salt():
+    """
+    Retrieve the salt from the salt file.
+    Returns the salt.
+    """
+    home_dir = Path.home()
+    db_dir = home_dir / DB_DIR_NAME
+    salt_file = db_dir / SALT_FILE_NAME
+    with open(salt_file, "rb") as f:
+        salt = f.read()
+    return salt
+
+def key_derivation_function(salt):
+    """
+    Define a key derivation function that uses the salt from the salt file.
+    Returns the key derivation function.
+    """
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=1_200_000,
+    )
+    return kdf
